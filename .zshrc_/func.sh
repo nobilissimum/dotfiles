912e41d796939af0987dd197ee522be6864f856a
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-27 22:50:20 +0800

    Refactor indents with 4 spaces

--- .zshrc_/func.sh
+++ .zshrc_/func.sh
@@ -1,31 +1,31 @@
 # Git
 girr () {
-  git reset --soft HEAD^
-  git restore --staged .
+    git reset --soft HEAD^
+    git restore --staged .
 }
 giwip () {
-  git add --all
-  git commit -m "WIP"
+    git add --all
+    git commit -m "WIP"
 }
 giss () {
-  d=$(date +%Y%m%d%H%M%S)
-  b=$(git branch --show-current)
-  git branch "$b-snapshot$d"
+    d=$(date +%Y%m%d%H%M%S)
+    b=$(git branch --show-current)
+    git branch "$b-snapshot$d"
 }
 gissc () {
-  giwip
-  giss
-  girr
+    giwip
+    giss
+    girr
 }
 gisse () {
-  d=$(date +%Y%m%d%H%M%S)
-  b=$(git branch --show-current)
-  git checkout -b "$b-snapshot$d"
+    d=$(date +%Y%m%d%H%M%S)
+    b=$(git branch --show-current)
+    git checkout -b "$b-snapshot$d"
 }
 gissb () {
-  d=$(date +%Y%m%d%H%M%S)
-  b=$(git branch --show-current)
-  git branch -m "$b-snapshot$d"
+    d=$(date +%Y%m%d%H%M%S)
+    b=$(git branch --show-current)
+    git branch -m "$b-snapshot$d"
 }
 
 # Environment
