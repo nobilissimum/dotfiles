2d7dbc3201196f83b11ef39575fa810633c89b81
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-27 21:40:25 +0800

    Update lualine sections coloring

--- .config/nvim/lua/nobilissimum/plugins/lualine.lua
+++ .config/nvim/lua/nobilissimum/plugins/lualine.lua
@@ -25,29 +25,43 @@ return {
                 a = { fg = colors.bg, bg = colors.blue, gui = "bold" },
                 b = { fg = colors.fg, bg = colors.bright_black },
                 c = { fg = colors.fg, bg = colors.hush_main },
+                x = { fg = colors.fg, bg = colors.bright_black },
+                y = { fg = colors.fg, bg = colors.hush_main },
             },
             command = {
                 a = { fg = colors.bg, bg = colors.yellow, gui = "bold" },
                 b = { fg = colors.fg, bg = colors.bright_black },
+                x = { fg = colors.fg, bg = colors.bright_black },
+                y = { fg = colors.fg, bg = colors.hush_main },
             },
             insert = {
                 a = { fg = colors.bg, bg = colors.green, gui = "bold" },
                 b = { fg = colors.fg, bg = colors.bright_black },
+                x = { fg = colors.fg, bg = colors.bright_black },
+                y = { fg = colors.fg, bg = colors.hush_main },
             },
             visual = {
                 a = { fg = colors.bg, bg = colors.magenta, gui = "bold" },
                 b = { fg = colors.fg, bg = colors.bright_black },
+                x = { fg = colors.fg, bg = colors.bright_black },
+                y = { fg = colors.fg, bg = colors.hush_main },
             },
             replace = {
                 a = { fg = colors.bg, bg = colors.red, gui = "bold" },
-                b = { fg = colors.fg, bg = colors.git },
+                b = { fg = colors.fg, bg = colors.bright_black },
+                x = { fg = colors.fg, bg = colors.bright_black },
+                y = { fg = colors.fg, bg = colors.hush_main },
             },
             inactive = {
                 a = { fg = colors.fg, bg = colors.bg, gui = "bold" },
                 b = { fg = colors.fg, bg = colors.bg },
                 c = { fg = colors.fg, bg = colors.bg },
+                x = { fg = colors.fg, bg = colors.bg },
+                y = { fg = colors.fg, bg = colors.bg },
             },
         }
+
+        local separators = { left = "", right = "" }
 
         require("lualine").setup({
             options = {
@@ -56,12 +70,35 @@ return {
                 section_separators = { left = "", right = "" },
             },
             sections = {
-                lualine_a = {{ "mode", separator = { left = "", right = "" } }},
+                lualine_a = {{ "mode", separator = separators } },
                 lualine_b = { "branch", "diff", "diagnostics" },
                 lualine_c = { "filename" },
-                lualine_x = { "encoding", "fileformat", "filetype" },
+                lualine_x = {
+                    {
+                        "encoding",
+                        padding = { left = 1, right = 2 },
+                        separator = separators,
+                    },
+                    {
+                        "fileformat",
+                        symbols = {
+                            unix = "LF",
+                            dos = "CRLF",
+                            mac = "CR",
+                        }
+                    },
+                    {
+                        "filetype",
+                        padding = { left = 2, right = 1 },
+                        separator = separators,
+                    },
+                },
                 lualine_y = { "progress" },
-                lualine_z = {{ "location", separator = { left = "", right = "" } }},
+                lualine_z = {{
+                    "location",
+                    padding = { left = 0, right = 1 },
+                    separator = separators,
+                }},
             },
         })
     end,
