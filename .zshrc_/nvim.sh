8f51e2ddfa17208c4a58d843311105ccacd17d8a
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-27 23:09:10 +0800

    Add telescope plugin to vim

--- .zshrc_/nvim.sh
+++ .zshrc_/nvim.sh
@@ -2,3 +2,15 @@ if ! luarocks --version $COMMAND &> /dev/null; then
     [[ "$OSTYPE" == "linux-gnu"* ]] && sudo apt install luarocks -y &> /dev/null
     [[ "$OSTYPE" == "darwin"* ]] && sudo brew install luarocks -y &> /dev/null
 fi
+
+
+if ! rg --version $COMMAND &> /dev/null; then
+    [[ "$OSTYPE" == "linux-gnu"* ]] && sudo apt install ripgrep -y &> /dev/null
+    [[ "$OSTYPE" == "darwin"* ]] && sudo brew install ripgrep -y &> /dev/null
+fi
+
+
+if ! fdfind --version $COMMAND &> /dev/null; then
+    [[ "$OSTYPE" == "linux-gnu"* ]] && sudo apt install fd-find -y &> /dev/null
+    [[ "$OSTYPE" == "darwin"* ]] && sudo brew install fd -y &> /dev/null
+fi
