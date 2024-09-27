b2672d21718ddda36ae6fc41d8efcf01863779bd
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-27 22:18:36 +0800

    Add treesitter plugin to vim

---
+++ .config/nvim/lua/nobilissimum/plugins/treesitter.lua
@@ -0,0 +1,30 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require("nvim-treesitter.configs").setup({
+            ensure_installed = {
+                "bash",
+                "c",
+                "html",
+                "javascript",
+                "json",
+                "lua",
+                "markdown",
+                "markdown_inline",
+                "python",
+                "query",
+                "regex",
+                "rust",
+                "toml",
+                "tsx",
+                "typescript",
+                "vim",
+                "vimdoc",
+                "yaml",
+            },
+            sync_install = false,
+            auto_install = true,
+            highlight = { enable = true },
+        })
+    end,
+}
