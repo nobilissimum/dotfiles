75d6b6b2500ce8393f6ad48554267653a81aaecd
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-27 23:16:32 +0800

    Fix tree-sitter dependencies

--- .zshrc_/nvim.sh
+++ .zshrc_/nvim.sh
@@ -1,9 +1,11 @@
+# Lazy
 if ! luarocks --version $COMMAND &> /dev/null; then
     [[ "$OSTYPE" == "linux-gnu"* ]] && sudo apt install luarocks -y &> /dev/null
     [[ "$OSTYPE" == "darwin"* ]] && sudo brew install luarocks -y &> /dev/null
 fi
 
 
+# Telescope
 if ! rg --version $COMMAND &> /dev/null; then
     [[ "$OSTYPE" == "linux-gnu"* ]] && sudo apt install ripgrep -y &> /dev/null
     [[ "$OSTYPE" == "darwin"* ]] && sudo brew install ripgrep -y &> /dev/null
@@ -13,4 +15,10 @@ fi
 if ! fdfind --version $COMMAND &> /dev/null; then
     [[ "$OSTYPE" == "linux-gnu"* ]] && sudo apt install fd-find -y &> /dev/null
     [[ "$OSTYPE" == "darwin"* ]] && sudo brew install fd -y &> /dev/null
+fi
+
+
+# Treesitter
+if ! tree-sitter --version $COMMAND &> /dev/null && npm --version $COMMAND &> /dev/null; then
+    npm install -g tree-sitter-cli
 fi
