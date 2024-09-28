2179ab9379a7373e8dfa8fccce4271dc92b0c205
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-28 11:47:59 +0800

    Use fuzzy search for telescope

--- .config/nvim/lua/nobilissimum/plugins/telescope.lua
+++ .config/nvim/lua/nobilissimum/plugins/telescope.lua
@@ -1,7 +1,31 @@
 return {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        {
+            "nvim-telescope/telescope-fzf-native.nvim",
+            build = "make",
+        },
+    },
     config = function()
+        require("telescope").setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    override_generic_sorter = true,
+                    override_file_sorter = true,
+                    case_mode = "smart_case",
+                }
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                },
+            },
+        })
+
+        require("telescope").load_extension("fzf")
+
         local telescope = require("telescope.builtin")
         vim.keymap.set("n", "<C-p>", telescope.find_files, { noremap = true, silent = true })
         vim.keymap.set("n", "<C-f>", telescope.live_grep, { noremap = true, silent = true })
