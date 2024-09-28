28d040dfdaa14e9f393e20d712ed75ba918ff06a
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-28 15:47:13 +0800

    Add mason plugin to vim

---
+++ .config/nvim/lua/nobilissimum/plugins/mason.lua
@@ -0,0 +1,15 @@
+return {
+    "williamboman/mason.nvim",
+    priority = 999,
+    config = function()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗",
+                },
+            },
+        })
+    end,
+}
