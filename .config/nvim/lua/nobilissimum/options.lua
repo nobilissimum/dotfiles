7eeef62b76216dc72bce93c80ef77a13041e0657
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-28 11:32:49 +0800

    Interchange j and k in vim

--- .config/nvim/lua/nobilissimum/options.lua
+++ .config/nvim/lua/nobilissimum/options.lua
@@ -6,3 +6,6 @@ vim.opt.softtabstop = 1
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
 vim.opt.autoindent = true
+
+vim.api.nvim_set_keymap("n", "j", "k", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "k", "j", { noremap = true, silent = true })
