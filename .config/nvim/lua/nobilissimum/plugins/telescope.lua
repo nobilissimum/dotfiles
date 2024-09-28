42fc63ec3166e391de5cdf3d91a92c58b98694b6
nobilissimum <ronn.angelo.lee@gmail.com>
2024-09-28 11:47:36 +0800

    Add keybinds for telescope find_files and live_grep

--- .config/nvim/lua/nobilissimum/plugins/telescope.lua
+++ .config/nvim/lua/nobilissimum/plugins/telescope.lua
@@ -1,4 +1,9 @@
 return {
     "nvim-telescope/telescope.nvim",
-    dependencies = { "nvim-lua/plenary.nvim" }
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+        local telescope = require("telescope.builtin")
+        vim.keymap.set("n", "<C-p>", telescope.find_files, { noremap = true, silent = true })
+        vim.keymap.set("n", "<C-f>", telescope.live_grep, { noremap = true, silent = true })
+    end,
 }
