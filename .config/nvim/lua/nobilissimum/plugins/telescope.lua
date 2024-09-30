1eefd9c212a2780498aeb9c436b66c0ecb5bb6ff
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-01 01:16:40 +0800

    Enable repository-wide string search

--- .config/nvim/lua/nobilissimum/plugins/telescope.lua
+++ .config/nvim/lua/nobilissimum/plugins/telescope.lua
@@ -29,6 +29,11 @@ return {
                 find_files = {
                     hidden = true,
                 },
+                live_grep = {
+                    additional_args = function()
+                        return { "--hidden" }
+                    end,
+                },
             },
         })
 
