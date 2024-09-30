5491a5f02c99d51a7324365c869f621fd8b2adbd
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-01 03:18:33 +0800

    Setup nvim LSPs with mason

--- .config/nvim/lua/nobilissimum/plugins/mason.lua
+++ .config/nvim/lua/nobilissimum/plugins/mason.lua
@@ -1,15 +1,53 @@
 return {
-    "williamboman/mason.nvim",
-    priority = 999,
-    config = function()
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup({
+                ui = {
+                    icons = {
+                        package_pending = " ",
+                        package_installed = " ",
+                        package_uninstalled = " ",
+                    },
+                    keymaps = {
+                        ["j"] = "k",
+                        ["k"] = "j",
+                    },
                 },
-            },
-        })
-    end,
+            })
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = true,
+            })
+
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    -- Lua
+                    "lua_ls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                    },
+                },
+            })
+        end,
+    },
 }
