0d9955cc69a0bb0810a555a68d4622d26a3c9a57
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-01 22:16:13 +0800

    Map keybinds for telescope
    Most of the keybinds here were referenced from
    github.com/nvim-lua/kickstart.nvim

--- .config/nvim/lua/nobilissimum/plugins/telescope.lua
+++ .config/nvim/lua/nobilissimum/plugins/telescope.lua
@@ -40,7 +40,15 @@ return {
         require("telescope").load_extension("fzf")
 
         local telescope = require("telescope.builtin")
-        vim.keymap.set("n", "<C-p>", telescope.find_files, { noremap = true, silent = true })
-        vim.keymap.set("n", "<C-f>", telescope.live_grep, { noremap = true, silent = true })
-    end,
+        vim.keymap.set("n", "<leader>sh", telescope.help_tags, { desc = "[S]earch [H]elp" })
+        vim.keymap.set("n", "<leader>sk", telescope.keymaps, { desc = "[S]earch [K]eymaps" })
+        vim.keymap.set("n", "<C-p>", telescope.find_files, { desc = "[S]earch [F]iles" })
+        vim.keymap.set("n", "<leader>ss", telescope.builtin, { desc = "[S]earch [S]elect Telescope" })
+        vim.keymap.set("n", "<leader>sw", telescope.grep_string, { desc = "[S]earch current [W]ord" })
+        vim.keymap.set("n", "<C-f>", telescope.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sd", telescope.diagnostics, { desc = "[S]earch [D]iagnostics" })
+        vim.keymap.set("n", "<leader>sr", telescope.resume, { desc = "[S]earch [R]esume" })
+        vim.keymap.set("n", "<leader>s.", telescope.oldfiles, { desc = "[S]earch recent files ('.' for repeat)" })
+        vim.keymap.set("n", "<leader><leader>", telescope.buffers, { desc = "[ ] Find existing buffers" })
+   end,
 }
