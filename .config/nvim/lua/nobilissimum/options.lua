fa34dc19b6edd1bf0a5804cc1c2958aa63d5992d
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-02 19:46:03 +0800

    Rearrange vim options

--- .config/nvim/lua/nobilissimum/options.lua
+++ .config/nvim/lua/nobilissimum/options.lua
@@ -1,28 +1,25 @@
+-- Number line
 vim.opt.number = true
 vim.opt.relativenumber = false
+vim.opt.signcolumn = "yes"
 
+-- Indentions
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 1
 vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
 vim.opt.autoindent = true
 
-local move_modes = { "n", "v", "s" }
-for _, move_mode in pairs(move_modes) do
-    vim.api.nvim_set_keymap(move_mode, "j", "k", { noremap = true, silent = true })
-    vim.api.nvim_set_keymap(move_mode, "k", "j", { noremap = true, silent = true })
-end
+vim.opt.breakindent = true
 
 vim.g.have_nerd_font = true
 
-vim.opt.breakindent = true
 
 vim.opt.undofile = true
 
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
-vim.opt.signcolumn = "yes"
 
 vim.opt.updatetime = 250
 
@@ -31,3 +28,10 @@ vim.opt.scrolloff = 10
 vim.opt.mouse = ""
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Movement keymaps
+local move_modes = { "n", "v", "s" }
+for _, move_mode in pairs(move_modes) do
+    vim.keymap.set(move_mode, "j", "k", { noremap = true, silent = true })
+    vim.keymap.set(move_mode, "k", "j", { noremap = true, silent = true })
+end
