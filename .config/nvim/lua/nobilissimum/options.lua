7f37b91734b56c367f756c070dbc009e73e78988
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-02 20:16:26 +0800

    Disable arrow key movements in NORMAL mode

--- .config/nvim/lua/nobilissimum/options.lua
+++ .config/nvim/lua/nobilissimum/options.lua
@@ -34,5 +34,12 @@ for _, move_mode in pairs(move_modes) do
 end
 
 
+-- Disable arrow keys in normal mode
+vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set("n", "<up>", '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set("n", "<down>", '<cmd>echo "Use k to move!!"<CR>')
+
+
 -- Diagnostics keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
