831d1b4ec17fdf9e27a92b3688ca152efb29bb05
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-02 22:46:52 +0800

    Configure mason installed LSPs automatically

--- .config/nvim/lua/nobilissimum/plugins/mason.lua
+++ .config/nvim/lua/nobilissimum/plugins/mason.lua
@@ -27,26 +27,17 @@ return {
                 update_in_insert = true,
             })
 
-            require("mason-lspconfig").setup({
-                ensure_installed = {
-                    -- Lua
-                    "lua_ls",
-                },
-            })
-
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                settings = {
-                    Lua = {
-                        diagnostics = {
-                            globals = { "vim" },
+            local language_server_configurations = {
+                lua_ls = {
+                    settings = {
+                        Lua = {
+                            diagnostics = {
+                                globals = { "vim" },
+                            },
                         },
                     },
                 },
-            })
-
-            if lspconfig.ruff_lsp then
-                lspconfig.ruff_lsp.setup({
+                ruff_lsp = {
                     init_options = {
                         settings = {
                             enable = true,
@@ -58,8 +49,20 @@ return {
                             },
                         },
                     },
-                })
-            end
+                },
+            }
+
+            require("mason-lspconfig").setup({
+                handlers = {
+                    function(language_server_name)
+                        local server_configuration = language_server_configurations[language_server_name]
+                        if not server_configuration then
+                            return
+                        end
+                        require("lspconfig")[language_server_name].setup(server_configuration)
+                    end,
+                },
+            })
         end,
     },
 }
