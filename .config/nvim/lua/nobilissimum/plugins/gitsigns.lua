a67fe2fa9539faeb0e5853c1f7063915a6735144
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-03 20:13:04 +0800

    Migrate from gitblame to gitsigns for inline blames

--- .config/nvim/lua/nobilissimum/plugins/gitsigns.lua
+++ .config/nvim/lua/nobilissimum/plugins/gitsigns.lua
@@ -1,10 +1,24 @@
 return {
     "lewis6991/gitsigns.nvim",
     config = function()
-        require("gitsigns").setup()
+        local padding = "     "
+        require("gitsigns").setup({
+            current_line_blame = true,
+            current_line_blame_opts = {
+                virt_text = true,
+                virt_text_pos = "eol",
+                delay = 0,
+                ignore_whitespace = true,
+                use_focus = true,
+            },
+            current_line_blame_formatter_nc = padding .. "Uncommitted change",
+            current_line_blame_formatter = padding .. "<abbrev_sha> • <author> • <author_time:%R> - <author_time:%Y%m%d:%H%M%S> • <summary>",
+        })
 
         vim.api.nvim_set_hl(0, "GitSignsAdd", { fg = "#65a884", bold = true })
         vim.api.nvim_set_hl(0, "GitSignsChange", { fg = "#74add2", bold = true })
         vim.api.nvim_set_hl(0, "GitSignsDelete", { fg = "#f77172", bold = true })
+
+        vim.api.nvim_set_hl(0, "GitSignsCurrentLineBlame", { fg = "#676b73" }) -- #ffffff33 on #1d232e
     end,
 }
