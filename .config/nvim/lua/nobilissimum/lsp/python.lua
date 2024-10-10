dbbe6e4584956b64f9c80aa6a1fb9dc7c1172c0e
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-10 18:45:28 +0800

    Remove unnecessary print

--- .config/nvim/lua/nobilissimum/lsp/python.lua
+++ .config/nvim/lua/nobilissimum/lsp/python.lua
@@ -2,7 +2,6 @@ vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("nobilissimum-ruff-hover", { clear = true }),
     callback = function(args)
         local client = vim.lsp.get_client_by_id(args.data.client_id)
-        print(client.name)
         if client == nil then
             return
         end
