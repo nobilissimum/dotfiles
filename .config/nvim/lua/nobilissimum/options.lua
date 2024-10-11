66992e67e201694589070d9835607fc1ed0979cb
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-11 13:15:41 +0800

    Enable movement by arrow keys

--- .config/nvim/lua/nobilissimum/options.lua
+++ .config/nvim/lua/nobilissimum/options.lua
@@ -47,10 +47,10 @@ end
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Disable arrow keys in normal mode
-vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
-vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
-vim.keymap.set("n", "<up>", '<cmd>echo "Use j to move!!"<CR>')
-vim.keymap.set("n", "<down>", '<cmd>echo "Use k to move!!"<CR>')
+-- vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
+-- vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
+-- vim.keymap.set("n", "<up>", '<cmd>echo "Use j to move!!"<CR>')
+-- vim.keymap.set("n", "<down>", '<cmd>echo "Use k to move!!"<CR>')
 
 
 -- Diagnostics keymaps
