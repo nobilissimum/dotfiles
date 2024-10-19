23ba0c5e17576921f7ebce4ef281a1b8d703a19f
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-19 23:26:29 +0800

    Refactor neovim

---
+++ .config/nvim/lua/nobilissimum/globals.lua
@@ -0,0 +1,22 @@
+Colors = {
+    black = "#0f1820",
+
+    hush = {
+        light = "#202733",
+        main = "#1d232e",
+        dark = "#121c26",
+    },
+
+    bright_black = "#535b68",
+    bright_black2 = "#393c4d",
+    bright_black3 = "#1f2a35",
+
+    red = "#f77172",
+    green = "#65a884",
+    yellow = "#cec999",
+    blue = "#74add2",
+    magenta = "#a980c4",
+    cyan = "#2d949f",
+
+    white = "#ffffff",
+}
--- .config/nvim/lua/nobilissimum/options.lua
+++ .config/nvim/lua/nobilissimum/options.lua
@@ -1,3 +1,8 @@
+-- Leader
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+
 -- Number line
 vim.opt.number = true
 vim.opt.relativenumber = false
@@ -38,12 +43,21 @@ vim.opt.scrolloff = 10
 -- Other keymaps
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
--- Disable arrow keys in normal mode
--- vim.keymap.set("n", "<left>", '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set("n", "<right>", '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set("n", "<up>", '<cmd>echo "Use j to move!!"<CR>')
--- vim.keymap.set("n", "<down>", '<cmd>echo "Use k to move!!"<CR>')
-
 
 -- Diagnostics keymaps
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+
+-- Clipboard
+vim.g.clipboard = {
+    name = "xclip",
+    copy = {
+        ["+"] = "xclip -selection clipboard",
+        ["*"] = "xclip -selection clipboard",
+    },
+    paste = {
+        ["+"] = "xclip -selection clipboard -o",
+        ["*"] = "xclip -selection clipboard -o",
+    },
+    cache_enabled = 0,
+}
