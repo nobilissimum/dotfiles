d2437b8797c9da8bf8115dade5156b0d34436406
nobilissimum <ronn.angelo.lee@gmail.com>
2024-10-20 12:34:24 +0800

    Add keybind to delete buffers through Telescope

--- .config/nvim/lua/nobilissimum/plugins/telescope.lua
+++ .config/nvim/lua/nobilissimum/plugins/telescope.lua
@@ -35,6 +35,9 @@ return {
             return layout
         end
 
+        local tl_builtin = require("telescope.builtin")
+        local tl_actions = require("telescope.actions")
+
         require("telescope").setup({
             defaults = {
                 sorting_strategy = "ascending",
@@ -42,6 +45,15 @@ return {
                     prompt_position = "top",
                 },
                 layout_strategy = "horizontal_merged",
+                mappings = {
+                    i = {
+                        ["<C-d>"] = tl_actions.delete_buffer,
+                    },
+                    n = {
+                        ["<C-d>"] = tl_actions.delete_buffer,
+                        ["dd"] = tl_actions.delete_buffer,
+                    },
+                },
             },
             extensions = {
                 fzf = {
@@ -70,17 +82,17 @@ return {
         require("telescope").load_extension("fzf")
 
         -- Keymaps
-        local telescope = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>sh", telescope.help_tags, { desc = "[S]earch [H]elp" })
-        vim.keymap.set("n", "<leader>sk", telescope.keymaps, { desc = "[S]earch [K]eymaps" })
-        vim.keymap.set("n", "<C-p>", telescope.find_files, { desc = "[S]earch [F]iles" })
-        vim.keymap.set("n", "<leader>ss", telescope.builtin, { desc = "[S]earch [S]elect Telescope" })
-        vim.keymap.set("n", "<leader>sw", telescope.grep_string, { desc = "[S]earch current [W]ord" })
-        vim.keymap.set("n", "<C-f>", telescope.live_grep, { desc = "[S]earch by [G]rep" })
-        vim.keymap.set("n", "<leader>sd", telescope.diagnostics, { desc = "[S]earch [D]iagnostics" })
-        vim.keymap.set("n", "<leader>sr", telescope.resume, { desc = "[S]earch [R]esume" })
-        vim.keymap.set("n", "<leader>s.", telescope.oldfiles, { desc = "[S]earch recent files ('.' for repeat)" })
-        vim.keymap.set("n", "<leader><leader>", telescope.buffers, { desc = "[ ] Find existing buffers" })
+        vim.keymap.set("n", "<leader>sh", tl_builtin.help_tags, { desc = "[S]earch [H]elp" })
+        vim.keymap.set("n", "<leader>sk", tl_builtin.keymaps, { desc = "[S]earch [K]eymaps" })
+        vim.keymap.set("n", "<C-p>", tl_builtin.find_files, { desc = "[S]earch [F]iles" })
+        vim.keymap.set("n", "<leader>ss", tl_builtin.builtin, { desc = "[S]earch [S]elect Telescope" })
+        vim.keymap.set("n", "<leader>sw", tl_builtin.grep_string, { desc = "[S]earch current [W]ord" })
+        vim.keymap.set("n", "<C-f>", tl_builtin.live_grep, { desc = "[S]earch by [G]rep" })
+        vim.keymap.set("n", "<leader>sd", tl_builtin.diagnostics, { desc = "[S]earch [D]iagnostics" })
+        vim.keymap.set("n", "<leader>sr", tl_builtin.resume, { desc = "[S]earch [R]esume" })
+        vim.keymap.set("n", "<leader>s.", tl_builtin.oldfiles, { desc = "[S]earch recent files ('.' for repeat)" })
+
+        vim.keymap.set("n", "<leader><leader>", tl_builtin.buffers, { desc = "[ ] Find existing buffers" })
 
 
         -- Colors
