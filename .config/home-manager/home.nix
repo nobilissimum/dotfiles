752e9515c3123620035b2eda4dccbfc68e28a4fe
nobilissimum <ronn.angelo.lee@gmail.com>
2024-11-07 02:35:23 +0800

    Add vim to nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -9,6 +9,7 @@
     packages = [
       pkgs.cowsay
       pkgs.lazygit
+      pkgs.vim
 
       # # It is sometimes useful to fine-tune packages, for example, by applying
       # # overrides. You can do that directly here, just don't forget the
