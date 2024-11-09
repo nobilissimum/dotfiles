69f87c6dbd3ff21a3bc695f3748b120417c02d7a
nobilissimum <ronn.angelo.lee@gmail.com>
2024-11-09 21:08:29 +0800

    Rename home configuration from tenshiro to linux

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -14,7 +14,7 @@
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."tenshiro" = home-manager.lib.homeManagerConfiguration {
+      homeConfigurations."linux" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
         modules = [ ./home.nix ];
 
