5a465b864513fda7566d92ff67049a3857cf67f1
nobilissimum <ronn.angelo.lee@gmail.com>
2024-11-11 22:06:08 +0800

    Remove unnecessary comments in nix home manager configuration

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -82,12 +82,6 @@ in
           chmod -R u+w $HOME/.config/nvim
         '';
       };
-
-      # # You can also set the file content immediately.
-      # ".gradle/gradle.properties".text = ''
-      #   org.gradle.console=verbose
-      #   org.gradle.daemon.idletimeout=3600000
-      # '';
     };
 
     sessionVariables = sessionVariables;
