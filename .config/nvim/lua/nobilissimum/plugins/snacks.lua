e92c1a87c923f4f9d077aa134746ec484b837bf5
nobilissimum <ronn.angelo.lee@gmail.com>
2024-12-11 02:05:05 +0800

    Setup features of snacks.nvim

--- .config/nvim/lua/nobilissimum/plugins/snacks.lua
+++ .config/nvim/lua/nobilissimum/plugins/snacks.lua
@@ -7,7 +7,38 @@ return {
         "nvim-tree/nvim-web-devicons",
     },
     config = function()
-        local snacks = require("snacks")
-        vim.keymap.set("n", "<leader>hb", snacks.git.blame_line, { desc = "Git [b]lame line" })
+        Snacks = require("snacks")
+        vim.keymap.set("n", "<leader>hb", Snacks.git.blame_line, { desc = "Git [b]lame line" })
+        vim.keymap.set("n", "<leader>un", Snacks.notifier.hide, { desc = "Dismiss all notifications" })
+
+        Snacks.setup({
+            animate = {
+                enabled = false,
+            },
+            bigfile = {
+                enabled = true,
+                notify = true,
+            },
+            notifier = {
+                enabled = true,
+                timeout = 30000,
+            },
+        })
+    end,
+    init = function()
+        vim.api.nvim_create_autocmd("User", {
+            pattern = "VeryLazy",
+            callback = function()
+                _G.dd = function(...)
+                    Snacks.debug.inspect(...)
+                end
+
+                _G.bt = function()
+                    Snacks.debug.backtrace()
+                end
+
+                vim.print = _G.dd
+            end,
+        })
     end,
 }
