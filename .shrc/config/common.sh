6859442ec9647dd46b006aac24d0323c8c4fec21
nobilissimum <ronn.angelo.lee@gmail.com>
2025-01-09 11:48:26 +0800

    Prevent shell cursor from using neovim cursor

--- .shrc/config/common.sh
+++ .shrc/config/common.sh
@@ -4,3 +4,11 @@ export HISTSIZE=5000
 # Editor
 export EDITOR=vim
 export VISUAL=vim
+
+echo -ne '\e]12;#c0c0c0\a'
+echo -ne '\e[4 q'
+function set_cursor_to_default() {
+    echo -ne '\e]12;#c0c0c0\a'
+    echo -ne '\e[4 q'
+}
+precmd_functions+=(set_cursor_to_default)
