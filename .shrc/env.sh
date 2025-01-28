4703892050ebfb607696b1106301cbb9b39a359f
nobilissimum <ronn.angelo.lee@gmail.com>
2025-01-28 20:40:20 +0800

    Add bun completion

--- .shrc/env.sh
+++ .shrc/env.sh
@@ -11,8 +11,8 @@ export POETRY_VIRTUALENVS_IN_PROJECT=1
 # Bun
 BUN_INSTALL="$HOME/.bun"
 if [ -d "$BUN_INSTALL" ]; then
-    export BUN_INSTALL="$BUN_INSTALL"
-    export PATH="$BUN_INSTALL/bin:$PATH"
+    export PATH="${BUN_INSTALL}/bin:$PATH"
+    source "$BUN_INSTALL/_bun"
 fi
 
 # .NET
