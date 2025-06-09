7a5743aeaf7718dfd245cb65a89abafee958f367
nobilissimum <ronn.angelo.lee@gmail.com>
2025-06-09 13:12:40 +0800

    Add validation for incorrect ascii image generation

--- .scripts/nvim-dash.sh
+++ .scripts/nvim-dash.sh
@@ -5,16 +5,17 @@ IMAGE_CACHE_DIRPATH="$HOME/.cache"
 
 width="${1:-30}"
 height="${2:-$((width/2.5))}"
+height="${height%.*}"
 
-filepath="$IMAGE_CACHE_DIRPATH/vimdash${width}x${height%.*}.txt"
+filepath="$IMAGE_CACHE_DIRPATH/vimdash${width}x${height}.txt"
 
 image=""
 if [ -f "$filepath" ]; then
     image=$(cat $filepath)
 fi
 
-if [[ -z "$image" ]]; then
-    image=$(ascii-image-converter  -C -c "$IMAGE_SRC_PATH" -d "$width,${height%.*}")
+if [[ -z "$image" || "$(wc -l < "$filepath")" -eq "${height}" ]]; then
+    image=$(ascii-image-converter  -C -c "$IMAGE_SRC_PATH" -d "$width,${height}")
     printf "%s" "$image" > "$filepath"
 fi
 
