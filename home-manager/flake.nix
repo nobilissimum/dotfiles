fdbef47228102a512339f5a3eb670711418521ea
nobilissimum <ronn.angelo.lee@gmail.com>
2025-06-11 01:15:49 +0800

    Format flake.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -1,25 +1,25 @@
 {
-  description = "Home Manager configuration";
+    description = "Home Manager configuration";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
 
-  outputs = { nixpkgs, home-manager, ... }:
+    outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in {
-      homeConfigurations."linux" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./home.nix ];
+        homeConfigurations."linux" = home-manager.lib.homeManagerConfiguration {
+            inherit pkgs;
+            modules = [ ./home.nix ];
 
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
-      };
+            # Optionally use extraSpecialArgs
+            # to pass through arguments to home.nix
+        };
     };
 }
