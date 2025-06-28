55318eb4f5141cb3f5b499e6d56d2d7846b514bc
nobilissimum <ronn.angelo.lee@gmail.com>
2025-06-28 23:45:00 +0800

    Refactor home manager modules without optionals

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -18,8 +18,10 @@
         homeConfigurations."nobi" = home-manager.lib.homeManagerConfiguration {
             inherit pkgs;
             modules = [ ./home.nix ]
-                ++ (pkgs.lib.optionals (pkgs.stdenv.system == "x86_64-linux") [ ./systems/x86_64-linux.nix ])
-                ++ (pkgs.lib.optionals (pkgs.stdenv.system == "x86_64-darwin") [ ./systems/x86_64-darwin.nix ])
+                ++ [ ./systems/${pkgs.stdenv.system}.nix ]
+
+                # ++ (pkgs.lib.optionals (pkgs.stdenv.system == "x86_64-linux") [ ./systems/x86_64-linux.nix ])
+                # ++ (pkgs.lib.optionals (pkgs.stdenv.system == "x86_64-darwin") [ ./systems/x86_64-darwin.nix ])
             ;
         };
     };
