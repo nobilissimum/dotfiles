5ba35e0c8c04bc57591e2ab73521733d5486021a
nobilissimum <ronn.angelo.lee@gmail.com>
2026-03-15 14:16:22 +0800

    Specify event for gitsigns

--- .config/nvim/lua/nobilissimum/plugins/gutter.lua
+++ .config/nvim/lua/nobilissimum/plugins/gutter.lua
@@ -1,6 +1,7 @@
 return {
     {
         "lewis6991/gitsigns.nvim",
+        event = { "BufReadPre", "BufNewFile" },
         config = function()
             local padding = "     "
             local gitsigns = require("gitsigns")
