4f36e66c9bf78278a2cd5b4c83ec18b202fb64e8
nobilissimum <ronn.angelo.lee@gmail.com>
2026-03-15 14:32:19 +0800

    Specify events for rainbow delimiters plugin

--- .config/nvim/lua/nobilissimum/plugins/rainbow-delimiters.lua
+++ .config/nvim/lua/nobilissimum/plugins/rainbow-delimiters.lua
@@ -1,5 +1,6 @@
 return {
     "HiPhish/rainbow-delimiters.nvim",
+    event = { "BufReadPre", "BufNewFile" },
     config = function()
         require("rainbow-delimiters.setup").setup({})
     end,
