98478ed22a7a8302fc355fcb226d05517c61fa09
nobilissimum <ronn.angelo.lee@gmail.com>
2026-03-26 00:39:13 +0800

    Update indent line events

--- .config/nvim/lua/nobilissimum/plugins/ibl.lua
+++ .config/nvim/lua/nobilissimum/plugins/ibl.lua
@@ -1,7 +1,7 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
-    event = { "BufReadPre", "BufNewFile" },
+    event = { "BufReadPost", "BufNewFile", "BufWinEnter" },
     config = function()
         local highlight = {
             "Indent",
